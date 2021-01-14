d5a33dd03d339b60410d06f14c95481ee1e5c8f5
juancriveros <juan.camilo.riveros@hotmail.com>
2021-01-13 22:59:09 -0500

    Consuming api for home page

--- routes/web/main.js
+++ routes/web/main.js
@@ -2,13 +2,36 @@ const express = require('express');
 const https = require('https');
 const http = require('http');
 const Router = express.Router();
-
+require('https').globalAgent.options.ca = require('ssl-root-cas').create();
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 Router.get('/', (req, res) => {
 
-    res.render('./main/home', {
-        
+
+    console.log("Ahora si")
+    https.get('https://localhost:44317/home/summary', (resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
     });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        console.log("LLEGO");
+        console.log(JSON.parse(data));
+        res.render('./main/home', {
+            data: JSON.parse(data)
+        });
+    });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err);
+    });
+
+
+    
 
 })
 
